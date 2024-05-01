da47a7f9b5e59dbc4156bfee1fb49d662013b68a
jiangvicc <binbinbb520@gmail.com>
2024-05-01 16:40:45 +0800

    新增後台產品彈窗元件

--- src/pages/admin/AdminProducts.js
+++ src/pages/admin/AdminProducts.js
@@ -1,12 +1,19 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
-
+import ProductModal from "../../components/ProductModal";
+import { Modal } from "bootstrap";
 
 function AdminProducts() {
   const [products, setProducts] = useState([]);
   const [pagination, setPagination] = useState({});
 
+  const productModal =useRef(null);
+
   useEffect(() => {
+    productModal.current = new Modal('#productModal', {
+      backdrop : 'static'
+    });
+
     (async () => {
       // 須注意這支 products api 才有分頁資訊，all api 沒有分頁資訊
       const productRes = await axios.get(`/v2/api/${process.env.REACT_APP_API_PATH}/admin/products`);
@@ -16,14 +23,24 @@ function AdminProducts() {
     })();
   }, [])
 
+  const openProductModal = () => {
+    productModal.current.show();
+  }
+
+  const closeProductModal = () => {
+    productModal.current.hide();
+  }
+
   return (
     <div className="p-3">
+      <ProductModal closeProductModal={closeProductModal} />
       <h3>產品列表</h3>
       <hr />
       <div className="text-end">
         <button
           type="button"
           className="btn btn-primary btn-sm"
+          onClick={openProductModal}
         >
           建立新商品
         </button>
